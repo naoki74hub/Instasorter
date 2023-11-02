2e5972769e50f94cb586ed6603fdaeebf8f0a49d
naoki74hub <nao.ryotaro.1112@icloud.com>
2023-11-03 03:18:37 +0900

    検索ボタンが押されたとき、親コンポーネントにイベントを発行

--- Instasorter/components/SearchBar.vue
+++ Instasorter/components/SearchBar.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <input type="text" placeholder="ユーザー名を入力してください">
-    <button @click="">検索</button>
+    <input type="text" v-model="username" placeholder="ユーザー名を入力してください">
+    <button @click="userSearch">検索</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, defineEmits} from 'vue'
+
+const username = ref('');
+const emit = defineEmits(['search'])
+const userSearch = () => {
+  emit('search', username.value);
+};
 </script>
