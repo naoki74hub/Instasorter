7b0137ae8fedf3588db86b1cbe1f6e4fbd3dd489
naoki74hub <nao.ryotaro.1112@icloud.com>
2023-11-03 01:10:31 +0900

    共通レイアウト

---
+++ Instasorter/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <header class="search-header">
+      <!-- 検索欄 -->
+      <input type="text">
+    </header>
+    <div class="main-content">
+      <aside class="sidebar">
+        <!-- ソートボタン配置場所　-->
+      </aside>
+      <main class="posts">
+        <!-- 検索されたユーザーの投稿(コードをレンダリングさせる部分→app.vueでいうところのNuxtPageと同じ役割) -->
+        <slot/>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
