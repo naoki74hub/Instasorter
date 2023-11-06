3d0c668ccba0a4f1e8dfc6adbf9eb74efb502560
naoki74hub <nao.ryotaro.1112@icloud.com>
2023-11-06 12:11:43 +0900

    after追加

--- Instasorter/server/api/instagram.ts
+++ Instasorter/server/api/instagram.ts
@@ -1,18 +1,18 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const username = body.username;
+  const after = body.after;
+
   //環境変数からアクセストークンとアカウントIDを取得
   const config = useRuntimeConfig()
   const baseUrl = config.privateRuntimeConfig.baseUrl
   const facebookAccountId = config.privateRuntimeConfig.accountId
-  const facebookAccessToken = config.privateRuntimeConfig.accessToken
+  const facebookAccessToken = config.privateRuntimeConfig.accessToken;
 
-  const url = `${baseUrl}${facebookAccountId}?fields=business_discovery.username(${username}){followers_count,media_count,media.limit(100){caption,media_url,permalink,timestamp,username,children{media_url}}}&access_token=${facebookAccessToken};`
-
+  const url = `${baseUrl}${facebookAccountId}?fields=business_discovery.username(${username}){followers_count,media_count,media.limit(10)${after ? `.after(${after})` : ''}{caption,media_url,permalink,timestamp,username,children{media_url}}}&access_token=${facebookAccessToken};`
   try {
     //InstagramグラフAPIに問い合わせを行う
     const response = await $fetch(url);
-    console.log(response);
     return response;
   
   } catch (error) {
