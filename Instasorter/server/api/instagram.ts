30c57af12e968c77f004ca5afe2c5931fa27d96f
naoki74hub <nao.ryotaro.1112@icloud.com>
2023-11-08 00:22:28 +0900

    useBodyをgetQueryに変更

--- Instasorter/server/api/instagram.ts
+++ Instasorter/server/api/instagram.ts
@@ -1,14 +1,14 @@
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  const username = body.username;
-  const after = body.after;
+  const query =  getQuery(event);
+  const username = query.username;
+  const after = query.after;
   //環境変数からアクセストークンとアカウントIDを取得
   const config = useRuntimeConfig()
   const baseUrl = config.privateRuntimeConfig.baseUrl
   const facebookAccountId = config.privateRuntimeConfig.accountId
   const facebookAccessToken = config.privateRuntimeConfig.accessToken;
 
-  const url = `${baseUrl}${facebookAccountId}?fields=business_discovery.username(${username}){followers_count,media_count,media.limit(10)${after ? `.after(${after})` : ''}{caption,media_url,permalink,timestamp,username,children{media_url}}}&access_token=${facebookAccessToken};`
+  const url = `${baseUrl}${facebookAccountId}?fields=business_discovery.username(${username}){followers_count,media_count,media.limit(100)${after ? `.after(${after})` : ''}{caption,media_url,permalink,timestamp,username,children{media_url}}}&access_token=${facebookAccessToken};`
   try {
     //InstagramグラフAPIに問い合わせを行う
     const response = await $fetch(url);
